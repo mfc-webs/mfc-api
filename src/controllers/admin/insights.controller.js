4bccd57c5ef0cbad419fa70fe1a3e78d57fb6668
Sibusiso Msimango <smsimango@gmail..com>
2026-03-23 17:28:46 +0200

    insights member chacked in today, and pagination for inactive users and checked in today

--- src/controllers/admin/insights.controller.js
+++ src/controllers/admin/insights.controller.js
@@ -5,20 +5,50 @@ import {
   getInactiveUsers,
   getActiveUsers,
   getAtRiskUsers,
-  getAvgVisits
+  getAvgVisits,
+  getTodayCheckIns,
+  getTodayCheckInsCount,
+  getInactiveUsersCount
 } from "../../services/insights.service.js";
 
 export const viewAdminInsights = async (req, res, next) => {
   try {
-    const [summary, goals, trend, inactiveUsers, activeMembers, atRiskMembers,  avgVisits ] = await Promise.all([
-      getSummaryStats(),
-      getGoalStats(),
-      getAttendanceTrend(),
-      getInactiveUsers(),
-      getActiveUsers(),
-      getAtRiskUsers(),
-      getAvgVisits()
-    ]);
+
+    const checkinsPage = parseInt(req.query.checkinsPage) || 1;
+    const inactivePage = parseInt(req.query.inactivePage) || 1;
+
+    const limit = 5;
+
+    const checkinsOffset = (checkinsPage - 1) * limit;
+    const inactiveOffset = (inactivePage - 1) * limit;
+
+   const [
+  summary,
+  goals,
+  trend,
+  activeMembers,
+  atRiskMembers,
+  avgVisits,
+  checkedIns,
+  totalCheckIns,
+  inactiveUsers,
+  totalInactiveUsers 
+] = await Promise.all([
+  getSummaryStats(),
+  getGoalStats(),
+  getAttendanceTrend(),
+  getActiveUsers(),                
+  getAtRiskUsers(),
+  getAvgVisits(),
+   getTodayCheckIns(limit, checkinsOffset),
+  getTodayCheckInsCount(),
+  getInactiveUsers(limit, inactiveOffset),
+  getInactiveUsersCount()
+]);
+
+    const totalCheckinPages = Math.ceil(totalCheckIns / limit);
+    const totalInactivePages = Math.ceil(totalInactiveUsers / limit);
+
 
     const engagementRate = summary.total_members > 0 ? ((activeMembers / summary.total_members) * 100).toFixed(1) : 0;
 
@@ -31,7 +61,13 @@ export const viewAdminInsights = async (req, res, next) => {
       activeMembers,
       engagementRate,
       atRiskMembers, 
-      avgVisits
+      avgVisits,
+      checkedIns,
+      // 👇 separate pagination
+      checkinsPage,
+      totalCheckinPages,
+      inactivePage,
+      totalInactivePages
     });
   } catch (err) {
     next(err);
